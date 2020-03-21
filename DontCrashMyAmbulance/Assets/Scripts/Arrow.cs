997bbde7ebe88d01bc17ac6d274cecd73432ef48
ktpunnisa <ktpunnisa@gmail.com>
2020-03-21 16:34:25 +0700

    clickable arrow

---
+++ DontCrashMyAmbulance/Assets/Scripts/Arrow.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Arrow : MonoBehaviour
+{
+    public Direction startArrow;
+    Direction arrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrow = startArrow;
+        setDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           if (Input.GetMouseButton(0)) {
+                updateDirection();
+           }
+    }
+
+    void setDirection()
+    {
+        switch (arrow)
+        {
+            case Direction.Up:
+                transform.localEulerAngles = new Vector3(0,0,90);
+                break;
+            case Direction.Down:
+                transform.localEulerAngles = new Vector3(0,0,270);
+                break;
+            case Direction.Left:
+                transform.localEulerAngles = new Vector3(0,0,180);
+                break;
+            case Direction.Right:
+                transform.localEulerAngles = new Vector3(0,0,0);
+                break;
+        }
+    }
+
+    void updateDirection() {
+        Direction newArrow = Direction.Up;
+        switch (arrow)
+        {
+            case Direction.Up:
+                newArrow = Direction.Left;
+                break;
+            case Direction.Down:
+                newArrow = Direction.Right;
+                break;
+            case Direction.Left:
+                newArrow = Direction.Down;
+                break;
+            case Direction.Right:
+                newArrow = Direction.Up;
+                break;
+        }
+        arrow = newArrow;
+        transform.Rotate( new Vector3( 0, 0, 90 ) );
+    }
+}
