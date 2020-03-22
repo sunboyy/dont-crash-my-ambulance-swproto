3458421376cb40cc0fb47a510bb9e9d7ac21cb15
sunboyy <sura_sun@hotmail.com>
2020-03-22 18:17:30 +0700

    Edit some naming on Junction.cs

--- DontCrashMyAmbulance/Assets/Scripts/Junction.cs
+++ DontCrashMyAmbulance/Assets/Scripts/Junction.cs
@@ -4,77 +4,74 @@ using UnityEngine;
 
 public class Junction : MonoBehaviour
 {
-    public Direction startArrow;
-    Direction arrow;
+    [SerializeField] Direction direction;
+
     Collider2D[] colliders;
     // Start is called before the first frame update
     void Start()
     {
-        arrow = startArrow;
-        setDirection();
+        RotateArrow();
     }
 
     // Update is called once per frame
     void Update()
     {
-         colliders = Physics2D.OverlapCircleAll(transform.position, 0.0f);
-         if (colliders.Length > 0){
-             ChangeDirection();
+         colliders = Physics2D.OverlapCircleAll(transform.position, 0f);
+         if (colliders.Length > 0) {
+             UpdateCarDirection();
          }
     }
 
-    void setDirection()
+    void RotateArrow()
     {
-        switch (arrow)
+        switch (direction)
         {
             case Direction.Up:
-                transform.localEulerAngles = new Vector3(0,0,90);
+                transform.localEulerAngles = new Vector3(0, 0, 90);
                 break;
             case Direction.Down:
-                transform.localEulerAngles = new Vector3(0,0,270);
+                transform.localEulerAngles = new Vector3(0, 0, 270);
                 break;
             case Direction.Left:
-                transform.localEulerAngles = new Vector3(0,0,180);
+                transform.localEulerAngles = new Vector3(0, 0, 180);
                 break;
             case Direction.Right:
-                transform.localEulerAngles = new Vector3(0,0,0);
+                transform.localEulerAngles = new Vector3(0, 0, 0);
                 break;
         }
     }
 
     void OnMouseDown() {
-        updateDirection();
+        UpdateDirection();
     }
 
-    void updateDirection() {
-        Direction newArrow = Direction.Up;
-        switch (arrow)
+    void UpdateDirection() {
+        switch (direction)
         {
             case Direction.Up:
-                newArrow = Direction.Left;
+                direction = Direction.Left;
                 break;
             case Direction.Down:
-                newArrow = Direction.Right;
+                direction = Direction.Right;
                 break;
             case Direction.Left:
-                newArrow = Direction.Down;
+                direction = Direction.Down;
                 break;
             case Direction.Right:
-                newArrow = Direction.Up;
+                direction = Direction.Up;
                 break;
         }
-        arrow = newArrow;
-        transform.Rotate( new Vector3( 0, 0, 90 ) );
+        RotateArrow();
     }
 
-    void ChangeDirection()
+    void UpdateCarDirection()
     {
         for(int i = 0; i < colliders.Length; i++) {
             if(colliders[i].name == "Ambulance") {
                 // Debug.Log(colliders[i].bounds.center);
                 // Debug.Log("arrow" + transform.position);
                 Ambulance car = colliders[i].GetComponent<Ambulance>();
-                car.ChangeDirection(arrow);
+                car.ChangeDirection(direction);
             }
         }
     }
